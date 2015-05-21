aa6564c5e9198a4a842ddcbf7d00377d381bbf21
Mark Lehman <markopolo@gmail.com>
2015-05-21 01:28:15 -0700

    begin deposit scraping setup

--- watir_automater.rb
+++ watir_automater.rb
@@ -6,11 +6,12 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 def main
   login
   transfer_amount = calculate_transfer_amount
-  transfer_funds
+  # transfer_funds
   
-binding.pry
-
   browser.close
+rescue => ex
+  browser.close
+  puts ex.inspect
 end
 
 def login
@@ -22,31 +23,35 @@ end
 
 def calculate_transfer_amount
   transactions = get_transaction_data
+# `transactions` returns an array of hashes - [{ deposit_date => deposit_amount }]
+
   # Calculate amounts of transactions to be transferred
+  # Need to store dates of last transactions transferred and exclude those amounts
+binding.pry
 end
 
 def get_transaction_data
-  # Here is where we inspect the transactions, search for transactions matching criteria, and return the amounts
+  # Search for transactions matching criteria, and return the dates and amounts
+  checking_account_link = browser.links.select{|link| link.text.include? 'Checking'}.first
+  checking_account_link.click
+  deposits = browser.trs.select{|tr| tr.text.include?('TECH 2000')}
+  deposit_dates_and_amounts = deposits.collect do |deposit|
+    { deposit.td(index: 0).text => deposit.td(index: 5).text }
+  end
 end
 
 def transfer_funds
-# Eventually should be dynamically created based on transaction data
-  transfer_amount = 0
-
-
   browser.link(:text, 'Transfers & Payments').click
   from_list_id = "#{prefix}selFromAccount"
   from_account_dropdown = browser.select_list(id: from_list_id)
-# This line takes an unreasonable amount of time to run... figure out a faster way to get this
-# Tried `from_account_dropdown.option(text: 'checking').select` but didn't work
   from_account = from_account_dropdown.options.select{|option| option.value.upcase.include? 'CHECKING'}.first.value
   from_account_dropdown.option(value: from_account).select
   to_list_id = "#{prefix}selToAccount"
   to_account_dropdown = browser.select_list(id: to_list_id)
   to_account = from_account_dropdown.options.select{|option| option.value.upcase.include? 'SAVING'}.first.value
   to_account_dropdown.option(value: to_account).select
-  amount_name = "#{prefix}txtTransToAmount"
-  browser.text_field(:name, amount_name).set(transfer_amount)
+  amount_id = "#{prefix}txtTransToAmount"
+  browser.text_field(:id, amount_id).set(transfer_amount)
   browser.link(:text, 'Continue').click
   browser.link(:id, "#{prefix}btnTransSubmit").click
 end
